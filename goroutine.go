3825687de239b951810d6bde7457b0a40f779813
SatheeshEppalapelli <eppalapellisatheesh1@gmail.com>
2020-03-04 15:06:35 +0530

    for loop

--- goroutine.go
+++ goroutine.go
@@ -5,11 +5,13 @@ import "fmt"
 func f(n int) {
 	  for i := 0; i < 10; i++ {
 		      fmt.Println(n, ":", i)
-		        }
+	      }
 		}
 
 		func main() {
-			  go f(0)
-			    var input string
-			      fmt.Scanln(&input)
-		      }
+			for i :=0; i < 10; i++{
+				go f(i)
+			}
+			var input string
+			fmt.Scanln(&input)
+		}
