f2a95e5732c6b2efff519539469e3ed2b3de4411
SatheeshEppalapelli <eppalapellisatheesh1@gmail.com>
2020-03-04 15:16:58 +0530

    add input value

--- goroutine.go
+++ goroutine.go
@@ -3,15 +3,17 @@ package main
 import "fmt"
 
 func f(n int) {
-	  for i := 0; i < 10; i++ {
-		      fmt.Println(n, ":", i)
-	      }
-		}
+		for i := 0; i < 10; i++ {
+					fmt.Println(n, ":", i)
+						}
+					}
 
-		func main() {
-			for i :=0; i < 10; i++{
-				go f(i)
-			}
-			var input string
-			fmt.Scanln(&input)
-		}
+					func main() {
+						        var value int
+							fmt.Scanln(&value)
+							for i := 0; i < value; i++ {
+										go f(i)
+											}
+												var input string
+													fmt.Scanln(&input)
+												}
